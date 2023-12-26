d7ce19591bd7af9c8f221a612d13b1d809e90229
Roland Foerther <roland@foerther.de>
2023-12-26 18:39:34 +0100

    cleanup code and update cmdline parser

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # killp
-Node.js module to kill a process running on given port or by its pid or kill its parent process (including children)
-
-WORK IN PROGRESS
+Node.js module to kill a process running on given port or kill its parent process (including children)
--- cli.mjs
+++ cli.mjs
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 'use strict'
 
-import killp from "./index.js";
+import killp from "./index.mjs";
 import yargs from "yargs";
 import {hideBin} from "yargs/helpers";
 
-
 const argv = yargs(hideBin(process.argv))
-    .usage('Usage: $0 [-v] -p num [--parent-name string]')
+	.usage('\nTerminate a process running on given port or its parent process\n\n' +
+		'Usage: $0 [-v] -p num [--parent-name string]')
+	.wrap(120)
+	.example('$0 -p 9000', 'terminate process which listens on port 9000')
+	.example('$0 -v -p 8080 --parent-name=zsh', 'terminate parent process of process which listens on port 8080 (verbose)')
 	.option('p', {alias: 'port', description: 'Port of process', demandOption: true, type: 'number'})
 	.option('parent-name',  {description: 'Name of the parent process', type: 'string'})
 	.option('v', {alias: 'verbose', description: 'Verbose output', type: 'boolean'})
+	.version()
     .argv
 
 const {port, parentName, verbose} = argv
