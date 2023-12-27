b329b8b157f2d129169f5f6f5be795408db02636
Roland Foerther <roland@foerther.de>
2023-12-27 23:00:06 +0100

    update doc

--- cli.mjs
+++ cli.mjs
@@ -6,13 +6,13 @@ import yargs from "yargs";
 import {hideBin} from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
-	.usage('\nTerminate a process running on given port or its parent process\n\n' +
-		'Usage: $0 [-v] -p num [--parent-name string]')
+	.usage('\nTerminates a process listening on a specific TCP port or terminates its parent process including children.\n')
+	.usage('Usage: $0 [-v] -p num [--parent-name string]')
 	.wrap(120)
-	.example('$0 -p 9000', 'terminate process which listens on port 9000')
-	.example('$0 -v -p 8080 --parent-name=zsh', 'terminate parent process of process which listens on port 8080 (verbose)')
-	.option('p', {alias: 'port', description: 'Port of process', demandOption: true, type: 'number'})
-	.option('parent-name',  {description: 'Name of the parent process', type: 'string'})
+	.example('$0 -v -p 9000', 'Terminates a process that is listening on port 9000 and show verbose output')
+	.example('$0 -p 8080 --parent-name=zsh', 'Terminates the parent process of a process that is listening on port 8080')
+	.option('p', {alias: 'port', description: 'Port of process to terminate', demandOption: true, type: 'number'})
+	.option('parent-name',  {description: 'Name of the parent process that is to be terminated instead', type: 'string'})
 	.option('v', {alias: 'verbose', description: 'Verbose output', type: 'boolean'})
 	.version()
     .argv
