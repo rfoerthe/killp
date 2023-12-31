3d139bdf4a965788bd3a68e4a304f280ed8ce773
Roland Foerther <roland@foerther.de>
2023-12-31 23:43:25 +0100

    Version 1.0.5: rename the parent-name option to ancestor. Allow list of allowed parent names.

--- cli.mjs
+++ cli.mjs
@@ -7,21 +7,25 @@ import {hideBin} from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
 	.usage('\nTerminates a process listening on a specific TCP port or terminates its parent process including children.\n')
-	.usage('Usage: $0 [-v] -p num [--parent-name string]')
-	.wrap(120)
+	.usage('Usage: $0 [-v] -p number [-a string[,string...]]')
+	.wrap(140)
 	.example('$0 -v -p 9000', 'Terminates a process that is listening on port 9000 and show verbose output')
-	.example('$0 -p 8080 --parent-name=zsh', 'Terminates the parent process of a process that is listening on port 8080')
+	.example('$0 -p 8080 -a "node,node.exe"', 'Terminates the parent process of a process that is listening on port 8080')
 	.option('p', {alias: 'port', description: 'Port of process to terminate', demandOption: true, type: 'number'})
-	.option('parent-name',  {description: 'Name of the parent process that is to be terminated instead', type: 'string'})
+	.option('a',  {alias: "ancestor",
+		description: 'Terminate parent process instead. Pass a comma-separated list of allowed parent process names.',
+		type: 'string'})
 	.option('f', {alias: 'force', description: 'Force terminating process', type: 'boolean'})
 	.option('v', {alias: 'verbose', description: 'Verbose output', type: 'boolean'})
 	.version()
+	.strict()
     .argv
 
-const {port, parentName, verbose, force} = argv
+const {port, ancestor, verbose, force} = argv
 
 try {
-	await killp(port, parentName, verbose, force)
+	const allowedParents = ancestor ? ancestor.split(",").map(item => item.trim()) : []
+	await killp(port, allowedParents, verbose, force)
 } catch (error) {
 	console.error(error.message)
 }
